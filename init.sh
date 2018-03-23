9163d615b8bd35f594db57b80af27f6b9c28b4b5
rubensa <rubensa@tluportatil076.treelogic.local>
2018-03-23 09:00:15 +0100

    Configure PostgreSQL Docker. Custom init_postgres.sh for PostgreSQ Docker image. Setup environment variables.

--- init.sh
+++ init.sh
@@ -6,6 +6,4 @@ git clone https://github.com/rubensa/Mattermost-LDAP.git Mattermost-LDAP
 
 # Move files to Dockerfile folders.
 mv Mattermost-LDAP/oauth app/
-mv Mattermost-LDAP/config_init.sh db/
-mv Mattermost-LDAP/init_postgres.sh db/
 rm -rf Mattermost-LDAP
